eb1852974e3fbe00ae453e9f5aac0334bcc36634
actypedef <actypedef@gmail.com>
2025-05-16 14:10:17 +0800

    added benchmark and mixed gemm

--- .ipynb_checkpoints/gemm-checkpoint.h
+++ .ipynb_checkpoints/gemm-checkpoint.h
@@ -78,3 +78,25 @@ float matmul_host8(
         const cutlass::float_ue8m0_t *SFA,
         const cutlass::float_ue8m0_t *SFB
 );
+
+float matmul_host(
+        const cutlass::float_e2m1_t *AN,
+        const cutlass::float_e2m1_t *BN,
+        const cutlass::float_e3m2_t *AS,
+        const cutlass::float_e2m1_t *BS,
+        const cutlass::float_e4m3_t *AO,
+        const cutlass::float_e2m1_t *BO,
+        int M,
+        int N,
+        int KN,
+        int KS,
+        int KO,
+        cutlass::bfloat16_t *C,
+        cutlass::bfloat16_t *D,
+        const cutlass::float_ue8m0_t *SFAN,
+        const cutlass::float_ue8m0_t *SFBN,
+        const cutlass::float_ue8m0_t *SFAS,
+        const cutlass::float_ue8m0_t *SFBS,
+        const cutlass::float_ue8m0_t *SFAO,
+        const cutlass::float_ue8m0_t *SFBO
+);
