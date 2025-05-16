e3fb269c115f697c298af5e878954eda1e35f56e
actypedef <actypedef@gmail.com>
2025-05-16 12:35:00 +0800

    fixed bench and added header files

---
+++ build/CMakeFiles/bench_fp4.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bench_fp4.dir/fp4.cu.o"
+  "CMakeFiles/bench_fp4.dir/fp4.cu.o.d"
+  "bench_fp4"
+  "bench_fp4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/bench_fp4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
